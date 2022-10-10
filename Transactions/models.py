135efb081d5a95854ec707a7f4617aca6dbb9dd9
paulovitor <pvitor7@hotmail.com>
2022-10-10 16:11:33 -0300

    feat: Banco de dados criado com tratamento de hora e data

--- Transactions/models.py
+++ Transactions/models.py
@@ -1,15 +1,13 @@
 import uuid
 from django.db import models
 
-from Store.models import Store
     
 class Transaction(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     type_transaction = models.IntegerField()
-    date = models.CharField(max_length=128)
-    hour = models.CharField(max_length=128)
-    value = models.DecimalField(decimal_places=2, max_digits=8)
+    date = models.DateField()
+    hour = models.TimeField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)
     cpf = models.CharField(max_length=128)
     card = models.CharField(max_length=128)
-    
-    store_id = models.ForeignKey(Store, on_delete=models.CASCADE)
+    store = models.ForeignKey('Store.Store', on_delete=models.CASCADE, related_name='transaction')
--- Transactions/serializers.py
+++ Transactions/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+
+from Store.serializers import StoreSerializer
 from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['type_transaction', 'date', 'hour', 'value', 'cpf', 'card']
+        fields = ['type_transaction', 'date', 'hour', 'value', 'cpf', 'card', 'store']
 
+    
